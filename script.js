4e706761519b752476d4529f4f83c53744b2087d
rapinadescolado <163008675+rapinadescolado@users.noreply.github.com>
2024-05-16 19:26:07 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function menuToggle(){
     const menu = document.getElementById('menu')
-    if(menu.style.right == -100vw){
+    if(menu.style.right != 0){
         menu.style.right = 0;
     }
     else{
