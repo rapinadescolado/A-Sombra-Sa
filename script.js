5a63bbb71dafc6552eb6c21d097bc78b0d2f497e
rapinadescolado <163008675+rapinadescolado@users.noreply.github.com>
2024-05-17 19:20:25 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 function menuToggle(){
     const menu = document.getElementById('menu');
     const button = document.getElementById('menubutton');
-    var buttontoggle = document.getElementById('exitbutton').style.opacity;
+    const buttonImg = document.getElementById('menubutton_img');
+    const exitbuttonImg = document.getElementById('exitbutton_img')
 
     if(menu.style.right != '0'){
         menu.style.right = '0';
         button.style.left = '10px';
+        buttonImg.style.opacity = '0';
+        exitbuttonImg.style.opacity = '1';
     }
     else{
         menu.style.right = '-100vw';
         button.style.left = '-55px';
-    }
-    if(buttontoggle == '0'){
-        buttontoggle = '1';
-    }
-    else{
-        buttontoggle = '0';
+        buttonImg.style.opacity = '1';
+        exitbuttonImg.style.opacity = '0';
     }
 }
