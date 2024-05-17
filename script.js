443ac717051ec5f1ef0e385ceab2d6c12a275519
rapinadescolado <163008675+rapinadescolado@users.noreply.github.com>
2024-05-17 17:33:16 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function menuToggle(){
     const menu = document.getElementById('menu');
     const button = document.getElementById('menubutton');
-    var buttontoggle = document.getElementById('exitmenubutton').style.opacity;
+    var buttontoggle = document.getElementById('exitbutton').style.opacity;
 
     if(menu.style.right =! '0'){
         menu.style.right = '0';
