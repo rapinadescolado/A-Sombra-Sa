05b4276e3fba56f8e5943a33dd9475da2b0c9722
rapinadescolado <163008675+rapinadescolado@users.noreply.github.com>
2024-05-17 19:46:32 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -7,12 +7,17 @@ function menuToggle(){
     if(menu.style.right == '-100vw'){
         menu.style.right = '0';
         button.style.left = '10px';
+        button.style.border-color = 'var(--bg_d1)'
+
         buttonImg.style.display = 'none';
         exitbuttonImg.style.display = 'block';
     }
     else{
         menu.style.right = '-100vw';
         button.style.left = '-55px';
+        button.style.border-color = 'var(--primary_d1)'
+
+
         buttonImg.style.display = 'block';
         exitbuttonImg.style.display = 'none';
     }
